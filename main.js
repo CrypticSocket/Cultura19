12d78a2c301b8e5dbf7f640baa94081bd156a022
Greymtr <greymtr@disroot.org>
2019-01-24 16:54:17 +0530

    blah

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ function animateCircles(event){
     var circle = document.createElement("div");
     circle.setAttribute("class", "circle");
     document.body.appendChild(circle);
-    
+
     circle.style.left = event.clientX + 'px';
     circle.style.top = event.clientY + 'px';
 
@@ -15,11 +15,15 @@ function animateCircles(event){
     circle.style.borderColor = colbd;
     circle.style.background = colbg;
     circle.style.transition = "all 0.5s linear 0s";
-    
+
     circle.style.right = circle.offsetLeft - 20 + 'px';
-    
-    circle.style.width = "80px";
-    circle.style.height= "80px";
+
+    circle.style.width = "20px";
+    circle.style.height= "20px";
 
     circle.style.opacity=0;
 }
+dip here !!!
+its working
+
+ yeahhh open up css? YOOOOOOOOO hi we can get started then.
