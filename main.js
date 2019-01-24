75d0e2de2d067dd05f52c98e709d929685622936
Greymtr <greymtr@disroot.org>
2019-01-24 18:07:50 +0530

    bleh

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 document.onmousemove = animateCircles;
 
-var colors = ['#fff', '#f00', '#0af'];
+var colors = ['#fff', '#f55', '#0af'];
 
 function animateCircles(event){
     var circle = document.createElement("div");
@@ -17,22 +17,42 @@ function animateCircles(event){
     circle.style.transition = "all 0.5s linear 0s";
 
     circle.style.right = circle.offsetLeft - 20 + 'px';
-<<<<<<< HEAD
 
     circle.style.width = "20px";
     circle.style.height= "20px";
-=======
->>>>>>> 06b4fbc8066f936eb6a6c68168174d96394fb468
 
-    circle.style.width = "30px";
-    circle.style.height= "30px";
-    
     circle.style.opacity=0;
 }
-<<<<<<< HEAD
-dip here !!!
-its working
 
- yeahhh open up css? YOOOOOOOOO hi we can get started then.
-=======
->>>>>>> 06b4fbc8066f936eb6a6c68168174d96394fb468
+//timer part
+
+//set the date HERE
+var countDownDate = new Date("Jan 31, 2019 02:35:00").getTime();
+
+//update teh countDown every 1 section
+var countdownfunction = setInterval(function(){
+  var now = new Date().getTime();
+
+  var distance = countDownDate - now;
+
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+
+  //output with id demo
+
+  document.getElementById("demo").innerHTML = days + "d    " + hours +  "h " + minutes + "m " + seconds + "s ";
+
+  //if countdown is over will write something HERE
+
+  if (distance < 0) {
+    clearInterval(countdownfunction);
+    document.getElementById("demo").innerHTML = "its on..blah blah!!";
+  }
+
+
+}, 1000);
+
+//wait dont change anything right now. il get confused.  XD
