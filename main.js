06b4fbc8066f936eb6a6c68168174d96394fb468
CrypticSocket <crypticsocket@gmail.com>
2019-01-24 16:52:49 +0530

    Added stuff

--- main.js
+++ main.js
@@ -6,20 +6,20 @@ function animateCircles(event){
     var circle = document.createElement("div");
     circle.setAttribute("class", "circle");
     document.body.appendChild(circle);
-    
+
     circle.style.left = event.clientX + 'px';
     circle.style.top = event.clientY + 'px';
 
-    var colbg = colors[Math.floor(Math.random() * colors.length)];
-    var colbd = colors[Math.floor(Math.random() * colors.length)];
-    circle.style.borderColor = colbd;
-    circle.style.background = colbg;
+    // var colbg = colors[Math.floor(Math.random() * colors.length)];
+    // var colbd = colors[Math.floor(Math.random() * colors.length)];
+    // circle.style.borderColor = colbd;
+    // circle.style.background = colbg;
     circle.style.transition = "all 0.5s linear 0s";
-    
-    circle.style.right = circle.offsetLeft - 20 + 'px';
-    
-    circle.style.width = "80px";
-    circle.style.height= "80px";
 
+    circle.style.right = circle.offsetLeft - 20 + 'px';
+
+    circle.style.width = "30px";
+    circle.style.height= "30px";
+    
     circle.style.opacity=0;
 }
